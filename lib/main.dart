6549a8e68a45232df1615d693ea90879796c37a1
NorhanWalid2 <walidnorhan2@gmail.com>
2024-11-02 23:07:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:book_app/Features/splash/presentation/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
