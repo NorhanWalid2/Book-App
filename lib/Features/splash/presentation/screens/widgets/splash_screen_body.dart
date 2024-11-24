cbb784c83d9cc4c2f22a73b2165894933b344330
NorhanWalid2 <walidnorhan2@gmail.com>
2024-11-24 14:59:28 +0200

    make navigation to home

--- lib/Features/splash/presentation/screens/widgets/splash_screen_body.dart
+++ lib/Features/splash/presentation/screens/widgets/splash_screen_body.dart
@@ -1,9 +1,12 @@
 import 'dart:ffi';
 
+import 'package:book_app/Features/home/presentation/screens/home_screen.dart';
 import 'package:book_app/Features/splash/presentation/screens/widgets/fading_logo.dart';
 import 'package:book_app/Features/splash/presentation/screens/widgets/sliding_text.dart';
 import 'package:book_app/core/utils/asset.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
@@ -21,19 +24,11 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 2),
-    );
-    opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
-    slideAnimation = Tween<Offset>(
-      begin: Offset(0, 4), // Starts off-screen at the bottom
-      end: Offset(0, 0), // Ends at the center
-    ).animate(
-      controller,
-    );
-    controller.forward();
+    initFadingAndSlidingAnimation();
+    navigateToHome();
   }
+
+  
 
   @override
   void dispose() {
@@ -52,5 +47,25 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
         SlidingText(slideAnimation: slideAnimation),
       ],
     );
+  }
+
+  void initFadingAndSlidingAnimation() {
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
+    slideAnimation = Tween<Offset>(
+      begin: Offset(0, 4), // Starts off-screen at the bottom
+      end: Offset(0, 0), // Ends at the center
+    ).animate(
+      controller,
+    );
+    controller.forward();
+  }
+  void navigateToHome() {
+    Future.delayed(Duration(seconds: 4), () {
+      Get.to(() => HomeScreen(),transition: Transition.leftToRight,duration: Duration(microseconds: 250));
+    });
   }
 }
