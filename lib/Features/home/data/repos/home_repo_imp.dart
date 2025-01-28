d3e8be2c3aeb491a5af1f76d3f70b26a0c934a58
NorhanWalid2 <walidnorhan2@gmail.com>
2025-01-28 07:09:13 +0200

    implement fetch newest books

--- lib/Features/home/data/repos/home_repo_imp.dart
+++ lib/Features/home/data/repos/home_repo_imp.dart
@@ -1,13 +1,26 @@
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/Features/home/data/repos/home_repos.dart';
 import 'package:book_app/core/errors/failures.dart';
+import 'package:book_app/core/utils/api_services.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImp implements HomeRepo{
+class HomeRepoImp implements HomeRepo {
+  final ApiServices apiServices;
+
+  HomeRepoImp({required this.apiServices});
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBook() {
-    // TODO: implement fetchBestSellerBook
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBook() async {
+    try {
+      var data = await apiServices.get(
+          endpoint: 'volumes?q=programming&orderBy=newest');
+      List<BookModel> Books = [];
+      for (var item in data['item']) {
+        Books.add(item);
+      }
+      return right(Books);
+    } catch (e) {
+      return left(ServiceFailure());
+    }
   }
 
   @override
@@ -15,5 +28,4 @@ class HomeRepoImp implements HomeRepo{
     // TODO: implement fetchFeatureBook
     throw UnimplementedError();
   }
-
 }
--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -3,7 +3,7 @@
 import 'package:dio/dio.dart';
 
 class ApiServices {
-  final _baseUrl = 'https://www.googleapis.com/books/v1/volumes';
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
   final Dio dio;
   ApiServices(this.dio);
   Future <Map<List, dynamic>> get({required String endpoint}) async{
