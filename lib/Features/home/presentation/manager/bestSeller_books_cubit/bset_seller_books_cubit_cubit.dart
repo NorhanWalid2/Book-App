97b0f14b16d77a858853ca04ab1470a3210d239d
NorhanWalid2 <walidnorhan2@gmail.com>
2025-02-12 00:30:13 +0200

    create best seller books method

--- lib/Features/home/presentation/manager/bestSeller_books_cubit/bset_seller_books_cubit_cubit.dart
+++ lib/Features/home/presentation/manager/bestSeller_books_cubit/bset_seller_books_cubit_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:book_app/Features/home/data/repos/home_repos.dart';
 import 'package:equatable/equatable.dart';
 
 part 'bset_seller_books_cubit_state.dart';
 
 class BsetSellerBooksCubitCubit extends Cubit<BsetSellerBooksCubitState> {
-  BsetSellerBooksCubitCubit() : super(BsetSellerBooksCubitInitial());
+  BsetSellerBooksCubitCubit(this.homeRepo)
+      : super(BsetSellerBooksCubitInitial());
+  final HomeRepo homeRepo;
+  Future<void> FetchBestSellerBooks() async {
+    emit(BsetSellerBooksCubitLoading());
+    var result = await homeRepo.fetchFeatureBook();
+    result.fold((failure) {
+      emit(BsetSellerBooksCubitFailure(errMessage: failure.errorMessage));
+    }, (books) {
+      emit(BsetSellerBooksCubitSuccess(books));
+    });
+  }
 }
